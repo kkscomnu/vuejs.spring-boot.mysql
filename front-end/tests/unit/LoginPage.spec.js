a983e775fbd4457cf7160b4cc19ee07553c1cf17
kkscomnu <kkscomnu@naver.com>
2021-02-26 04:08:36 +0900

    combine build process

--- front-end/tests/unit/LoginPage.spec.js
+++ front-end/tests/unit/LoginPage.spec.js
@@ -4,8 +4,8 @@ import LoginPage from '@/views/LoginPage'
 describe('LoginPage.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(LoginPage)
-      const vm = new Constructor().$mount()
-      expect(vm.$el.querySelector('h1').textContent)
-        .toEqual('TaskAgile')
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('h1').textContent)
+      .toEqual('TaskAgile')
   })
 })
