620dd502637c6b3980e0a76deae7a617788c0600
kkscomnu <kkscomnu@naver.com>
2021-03-02 00:17:36 +0900

    build registration form

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = '/api'
+axios.defaults.headers.common.Accept = 'applicaiton/json'
+axios.interceptors.response.use(
+  response => response,
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 new Vue({
   router,
---
+++ front-end/src/views/RegisterPage.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="register-form">
+        <div class="logo-wrapper">
+          <img class="logo" src="/static/images/logo.png">
+          <div class="tagline">Open source task management tool</div>
+        </div>
+        <form @submit.prevent="submitForm">
+          <div v-show="errorMessage" class="alert alert-danger failed">{{ errorMessage }}</div>
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-contorl" id="username" v-model="form.username">
+          </div>
+          <div class="form-group">
+            <label for="emailAddress">Email address</label>
+            <input type="email" class="form-contorl" id="emailAddress" v-model="form.emailAddress">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-contorl" id="password" v-model="form.password">
+          </div>
+          <button type="submit" class="btn btn-primary btn-block">Create account</button>
+        </form>
+      </div>
+    </div>
+    <footer class="footer">
+      <span class="copyright">&copy; 2018 TaskAgile.com</span>
+      <ul class="footer-links list-inline float-right">
+        <li class="list-inline-item"><a href="#">About</a></li>
+        <li class="list-inline-item"><a href="#">Terms of Service</a></li>
+        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+        <li class="list-inline-item"><a href="https://github.com/taskagile/vuejs.spring-boot.mysql" target="_blank">GitHub</a></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<script>
+import registrationService from '@/services/registration'
+
+export default {
+  name: 'RegisterPage',
+  data: function () {
+    return {
+      form: {
+        username: '',
+        emailAddress: '',
+        password: ''
+      },
+      errorMessage: ''
+    }
+  },
+  methods: {
+    submitForm () {
+      registrationService.register(this.form).then(() => {
+        this.$router.push({ name: 'LoginPage' })
+      }).catch((error) => {
+        this.errorMessage = 'Failed to register user. Reason: ' + (error.message ? error.message : 'Unknown') + '.'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container {max-width: 900px;}
+  .register-form {margin-top: 50px; max-width: 320px;}
+  .logo-wrapper {margin-bottom: 40px;}
+  .footer {width: 100%; line-height: 40px; margin-top: 50px;}
+</style>
